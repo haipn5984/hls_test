f6700fd9d2fee38c7d307c221dd7df347e524d16
Phạm Ngọc Hải <haipn@ezcloud.vn>
2024-08-04 17:41:52 +0700

    solution func

--- algorithm/algorithm.dart
+++ algorithm/algorithm.dart
@@ -1,7 +1,8 @@
 import 'dart:io';
 
 void main() {
-  inputList();
+  clearScreen();
+  listProcess(inputList());
 }
 
 List<int> inputList([retry = false]) {
@@ -23,5 +24,59 @@ List<int> inputList([retry = false]) {
     }
   } catch (e) {
     return inputList(true);
+  }
+}
+
+void clearScreen() {
+  if (Platform.isWindows) {
+    print(Process.runSync('cls', [], runInShell: true).stdout);
+  } else {
+    print(Process.runSync('clear', [], runInShell: true).stdout);
+  }
+}
+
+double getSum(List<int> input, {int numberOfE = 4}) {
+  double sum = 0;
+  for (var i = 0; i < numberOfE; i++) {
+    sum += input[i];
+  }
+  return sum;
+}
+
+void listProcess(List<int> list) {
+  list.sort();
+  sumCalculate(list);
+  listInfo(list);
+}
+
+void sumCalculate(List<int> list) {
+  try {
+    print('Minimum sum of 4 of 5 elements: ${getSum(list)}');
+    print('Maximum sum of 4 of 5 elements: ${getSum(
+      list.reversed.toList(),
+    )}');
+  } catch (e) {
+    print(e);
+  }
+}
+
+void listInfo(List<int> list) {
+  try {
+    final total = getSum(list, numberOfE: 5);
+    print('Total of array $total');
+
+    final min = list.first;
+    print('Min in array $min');
+
+    final max = list.last;
+    print('Max in array $max');
+
+    List<int> evenE = list.where((element) => element % 2 == 0).toList();
+    print('Even elements: $evenE');
+
+    List<int> oddE = list.where((element) => element % 2 != 0).toList();
+    print('Odd elements $oddE');
+  } catch (e) {
+    print(e);
   }
 }
